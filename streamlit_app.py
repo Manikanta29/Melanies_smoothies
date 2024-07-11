78bc3e8aad582224efbf92830fcab3c6e454f18e
Manikanta <265mani@gmail.com>
2024-07-11 17:24:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 # Import python packages
-import snowflake as sf
+#import snowflake as sf
 #import snowflake.snowpark.python as ss
 import streamlit as st
 from snowflake.snowpark.functions import col
